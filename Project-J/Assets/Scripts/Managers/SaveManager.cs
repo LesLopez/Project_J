87c9444029aa1b0612bb1af1c22384fc4827a9f7
Théophile <thophilecandelier@yahoo.fr>
2020-02-08 16:33:26 +0100

    Created project hierarchy

---
+++ Project-J/Assets/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : Singleton<SaveManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project-J/Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    public static T instance;
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance.gameObject);
+        }
+        instance = GetComponent<T>();
+    }
+}
